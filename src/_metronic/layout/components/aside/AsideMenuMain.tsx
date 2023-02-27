57b6d5386e0478730d0a315787903b1f5e87cdba
Kofi K <kofivincent026@gmail.com>
2023-02-27 15:59:49 +0000

    Leading icons added for AsideMenuItems

--- src/_metronic/layout/components/aside/AsideMenuMain.tsx
+++ src/_metronic/layout/components/aside/AsideMenuMain.tsx
@@ -8,42 +8,58 @@ export function AsideMenuMain() {
 
   return (
     <>
-      <AsideMenuItemWithSub to='' title='MAIN NAVIGATION' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen022.svg'>
-        <AsideMenuItem to='register' icon='/media/icons/duotune/general/gen055.svg' title='Membership' />
-        <AsideMenuItem to='planning' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen014.svg' title='Calendar' />
-        <AsideMenuItem to='tee-sheet' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen022.svg' title='Tee Sheet' />
-        <AsideMenuItem to='gameplay' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen022.svg' title='Gameplay' />
-        <AsideMenuItem to='shop' fontIcon='bi-archive' icon='/media/icons/duotune/ecommerce/ecm001.svg' title='Shop' />
-        {/*<AsideMenuItem to='pos' fontIcon='bi-archive' icon='/media/icons/duotune/finance/fin007.svg' title='POS' />*/}
-        {/*<AsideMenuItem to='partners' fontIcon='bi-archive' icon='/media/icons/duotune/technology/teh010.svg' title='Partners' />*/}
-        {/*<AsideMenuItem to='card-management' fontIcon='bi-archive' icon='/media/icons/duotune/finance/fin002.svg' title='Card Management' />*/}
+        <AsideMenuItemWithSub to='' title='MAIN NAVIGATION' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen022.svg'>
+            <AsideMenuItem to='register' icon='/media/icons/duotune/general/gen055.svg' title='Membership' />
+            <AsideMenuItem to='planning' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen014.svg' title='Calendar' />
+            <AsideMenuItem to='tee-sheet' fontIcon='bi-archive' icon='/media/icons/duotune/ecommerce/ecm009.svg' title='Tee Sheet' />
+            <AsideMenuItem to='gameplay' fontIcon='bi-archive' icon='/media/icons/duotune/communication/com001.svg' title='Gameplay' />
+            <AsideMenuItem to='shop' fontIcon='bi-archive' icon='/media/icons/duotune/ecommerce/ecm001.svg' title='Shop' />
+            {/*<AsideMenuItem to='pos' fontIcon='bi-archive' icon='/media/icons/duotune/finance/fin007.svg' title='POS' />*/}
+            {/*<AsideMenuItem to='partners' fontIcon='bi-archive' icon='/media/icons/duotune/technology/teh010.svg' title='Partners' />*/}
+            {/*<AsideMenuItem to='card-management' fontIcon='bi-archive' icon='/media/icons/duotune/finance/fin002.svg' title='Card Management' />*/}
 
-      </AsideMenuItemWithSub>
-      <div className='menu-item'>
-        <div className='menu-content'>
-          <div className='separator mx-1 my-4'></div>
+        </AsideMenuItemWithSub>
+        <div className='menu-item'>
+            <div className='menu-content'>
+                <div className='separator mx-1 my-4'></div>
+            </div>
         </div>
-      </div>
-      <AsideMenuItemWithSub
-        to='#'
-        title='Reports'
-        fontIcon='bi-archive'
-        icon='/media/icons/duotune/coding/cod009.svg'
-      >
-        <AsideMenuItem to='/reports/members-report' title='Members report' hasBullet={true} />
-      </AsideMenuItemWithSub>
-      <AsideMenuItemWithSub
-        to='#'
-        title='Setup'
-        fontIcon='bi-archive'
-        icon='/media/icons/duotune/coding/cod009.svg'
-      >
-        <AsideMenuItem to='/setup/course-setup' title='Course Setup' hasBullet={true} />
-        <AsideMenuItem to='/setup/fees' fontIcon='bi-archive' icon='/media/icons/duotune/finance/fin007.svg' title='Fees Setup' />
-        <AsideMenuItem to='/setup/caddies' title='Caddies' hasBullet={true} />
+        <AsideMenuItemWithSub
+            to='#'
+            title='Reports'
+            fontIcon='bi-archive'
+            icon='/media/icons/duotune/general/gen005.svg'>
+            <AsideMenuItem
+                to='/reports/members-report'
+                title='Members report'
+                fontIcon='bi-archive'
+                icon='/media/icons/duotune/communication/com014.svg'
+                hasBullet={false} />
+        </AsideMenuItemWithSub>
+        <AsideMenuItemWithSub
+            to='#'
+            title='Setup'
+            fontIcon='bi-archive'
+            icon='/media/icons/duotune/coding/cod009.svg'
+        >
+            <AsideMenuItem
+                to='/setup/course-setup'
+                title='Course Setup'
+                fontIcon='bi-archive'
+                icon='/media/icons/duotune/maps/map003.svg'
+                hasBullet={false} />
+            <AsideMenuItem
+                to='/setup/fees'
+                fontIcon='bi-archive'
+                icon='/media/icons/duotune/finance/fin010.svg' title='Fees Setup' />
+            <AsideMenuItem
+                to='/setup/caddies'
+                title='Caddies'
+                fontIcon='bi-archive'
+                icon='/media/icons/duotune/communication/com013.svg'
+                hasBullet={false} />
 
-      </AsideMenuItemWithSub>
-
+        </AsideMenuItemWithSub>
     </>
   )
 }
