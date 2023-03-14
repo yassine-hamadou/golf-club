fef3b91d3be75e00cc070e055386e0c705930498
Yassine Hamadou <yassinehamadou1@gmail.com>
2023-03-14 11:50:47 +0000

    Added Game Type POst functionnality

--- src/app/modules/production/components/teeSheet/setup/GameTypeTable.tsx
+++ src/app/modules/production/components/teeSheet/setup/GameTypeTable.tsx
@@ -1,15 +1,76 @@
 import {PageTitle} from '../../../../../../_metronic/layout/core'
 import {KTCard, KTCardBody, KTSVG} from '../../../../../../_metronic/helpers'
-import {Button, Input, Space, Table} from 'antd'
-import {Link} from 'react-router-dom'
+import {Button, Form, Input, message, Modal, Space, Table} from 'antd'
+import {useMutation, useQuery, useQueryClient} from "react-query";
+import axios from "axios";
+import {API_URL} from "../../../../../urls";
+import {useState} from "react";
+import TextArea from "antd/lib/input/TextArea";
 
 export const GameTypeTable = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [submitLoading, setSubmitLoading] = useState(false)
+
+  const {data: gameTypes} = useQuery('gameTypes', () => axios.get(`${API_URL}/gameTypes`))
+  console.log("Game Types", gameTypes)
+  const [form] = Form.useForm()
+
+
+  ///////////////////////////////////
+  // add modal functions  ///////////
+  ///////////////////////////////////
+  const queryClient = useQueryClient()
+  const {mutate: addGameType} = useMutation((values: any) => axios.post(`${API_URL}/gameTypes`, values))
+
+  function handleCancel() {
+    form.resetFields()
+    setIsModalOpen(false)
+  }
+  const onFinish = async (values: any) => {
+    setSubmitLoading(true)
+    addGameType(values, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('gameTypes')
+        message.success('Game Type added successfully')
+        form.resetFields()
+        setSubmitLoading(false)
+        setIsModalOpen(false)
+      },
+      onError: (error: any) => {
+        message.error(error.message)
+        setSubmitLoading(false)
+      }
+    })
+  }
+
+
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+///////////////////////////////////
+// End add modal functions  ///////////
+///////////////////////////////////
   const columns: any = [
     {
       title: 'ID',
+      dataIndex: 'id',
     },
     {
       title: 'Game Type',
+      dataIndex: 'name',
+      sorter: (a: any, b: any) => {
+        if (a.txmanf > b.txmanf) {
+          return 1
+        }
+        if (b.txmanf > a.txmanf) {
+          return -1
+        }
+        return 0
+      },
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
       sorter: (a: any, b: any) => {
         if (a.txmanf > b.txmanf) {
           return 1
@@ -22,6 +83,14 @@ export const GameTypeTable = () => {
     },
     {
       title: 'Action',
+        dataIndex: 'action',
+        render: (text: any, record: any) => (
+            <Space size='middle'>
+                    <button type='button' className='btn btn-danger me-3'>
+                        Delete
+                    </button>
+            </Space>
+        ),
     },
   ]
   return (
@@ -41,15 +110,52 @@ export const GameTypeTable = () => {
               <Button type='primary'>Search</Button>
             </Space>
             <Space style={{marginBottom: 16}}>
-              <Link to='add'>
-                <button type='button' className='btn btn-primary me-3'>
-                  <KTSVG path='/media/icons/duotune/arrows/arr075.svg' className='svg-icon-2' />
-                  Add
-                </button>
-              </Link>
+              <button type='button' className='btn btn-primary me-3' onClick={showModal}>
+                <KTSVG path='/media/icons/duotune/arrows/arr075.svg' className='svg-icon-2' />
+                Add New Game Type
+              </button>
             </Space>
           </div>
-          <Table columns={columns} bordered />
+          <Table columns={columns} bordered dataSource={gameTypes?.data}/>
+          <Modal
+            title='Add Game Type'
+            open={isModalOpen}
+            onCancel={handleCancel}
+            closable={true}
+            footer={[
+              <Button key='back' onClick={handleCancel}>
+                Cancel
+              </Button>,
+              <Button
+                key='submit'
+                type='primary'
+                htmlType='submit'
+                loading={submitLoading}
+                onClick={() => {
+                  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                  form.submit()
+                }}
+              >
+                Submit
+              </Button>,
+            ]}
+          >
+            <Form
+              form={form}
+              name='control-hooks'
+              labelCol={{span: 8}}
+              wrapperCol={{span: 14}}
+              title='Add Game Type'
+              onFinish={onFinish}
+            >
+              <Form.Item name='name' label='Name of the Game Type'>
+                <Input style={{color: 'black'}} />
+              </Form.Item>
+              <Form.Item name='description' label='Description of the Game Type'>
+                <TextArea style={{color: 'black'}} />
+              </Form.Item>
+            </Form>
+          </Modal>
         </KTCardBody>
       </KTCard>
     </>
