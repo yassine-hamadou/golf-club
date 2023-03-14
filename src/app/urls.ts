7a33647ca4c5935188890442f80feca4ce5fc509
Yassine Hamadou <yassinehamadou1@gmail.com>
2023-03-14 17:24:00 +0000

    added server api

--- src/app/urls.ts
+++ src/app/urls.ts
@@ -1,4 +1,5 @@
  // Use this file to define your base URLs whether on localhost or on the server
  // export const BASE_URL = 'http://localhost:3001';
  export const BASE_URL = 'https://egolf.onrender.com';
- export const API_URL = 'https://localhost:44397/api';
+ // export const API_URL = 'https://localhost:44397/api';
+ export const API_URL = 'https://173.248.129.77/newEgolfApi/api';
